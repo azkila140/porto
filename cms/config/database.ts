f35da85d6812fe06c5d5085e6c2812a03bc8ff66
Nexus Logic <nexuslogic@agency.com>
2026-02-08 21:19:20 +0100

    fix: resolve all typescript errors in cms and strapi-cms folders

--- cms/config/database.ts
+++ cms/config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default ({ env }: { env: any }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'sqlite') as 'mysql' | 'postgres' | 'sqlite';
 
   const connections = {
     mysql: {
